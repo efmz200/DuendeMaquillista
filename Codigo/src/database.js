b68eab170736baf487fa190cb994593306b77e10
Erick <efmz2000@outlook.es>
2023-10-12 15:31:49 -0600

    recuperacion de contraseña

--- Codigo/src/database.js
+++ Codigo/src/database.js
@@ -4,9 +4,6 @@ require('dotenv').config();
 const user = process.env.MONGO_USER;
 const password = process.env.MONGO_PASSWORD;
 const dbname = process.env.MONGO_DB;
-//const user = "Manejador_codigo";
-//const password = "oeDnZXIYmQevbOAO";
-//const dbname = "DuendeMaquillista"
 
 
 const URI = `mongodb+srv://${user}:${password}@cluster0.swfljtx.mongodb.net/${dbname}?retryWrites=true&w=majority`;
--- Codigo/src/routes/admUsuario.routes.js
+++ Codigo/src/routes/admUsuario.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router =express.Router();
-
+require('dotenv').config();
+const nodemailer = require('nodemailer');
 const Usuario = require('../models/modelo/Usuario');
 
 router.get('/',async (req,res) => {
-    const usuario =await Usuario.find({})
+    const usuario = await Usuario.find({})
     console.log(usuario)
     res.json(usuario)
 })
@@ -57,10 +58,11 @@ router.post('/registrarUsuario', async (req,res) =>{
     }
 });
 
+//actualiza un usuario si concuerda el correo y la contraseña
 router.post('/actualizarUsuario', async (req,res) =>{
     try{
-        const {nombre,apellido,correoAntiguo,correo,contrasena,nacimiento,telefono,genero} = req.body;
-        const usuario = await Usuario.findOneAndUpdate({correo: correoAntiguo},
+        const {nombre,apellido,correoAntiguo,constasenaAntigua,correo,contrasena,nacimiento,telefono,genero} = req.body;
+        const usuario = await Usuario.findOneAndUpdate({correo: correoAntiguo,contrasena: constasenaAntigua},
         {
             nombre,
             apellido,
@@ -82,11 +84,69 @@ router.post('/actualizarUsuario', async (req,res) =>{
     }
 })
 
-
-router.post('/eliminarUsuario', async (req,res) =>{
+router.post('/olvideContrasena', async (req,res) =>{
     try{
         const {correo} = req.body;
-        await Usuario.findOneAndDelete({ correo });
+        console.log(process.env.GMAIL_MAIL)
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+              type: 'OAuth2',
+              user: process.env.GMAIL_MAIL,
+              pass: process.env.GMAIL_PASSWORD,
+              clientId: process.env.GMAIL_CLIENT_ID,
+              clientSecret: process.env.GMAIL_CLIENT_SECRET,
+              refreshToken: process.env.GMAIL_REFRESH_TOKEN
+            }
+          });
+
+        const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+';
+        let contrasena = '';
+        for (let i = 0; i < 12; i++) {
+            const indice = Math.floor(Math.random() * caracteres.length);
+            contrasena += caracteres[indice];
+        }
+        
+        const mensaje = `Su nueva contraseña es: ${contrasena}. \nSi usted no solicitó el cambio de contraseña, cámbiela inmediatamente.`;
+  
+        const mailOptions = {
+            from: process.env.GMAIL_MAIL,
+            to: correo,
+            subject: 'Cambio de contrasenia',
+            text: mensaje
+        };
+          
+        const usuario = await Usuario.findOneAndUpdate({correo},{contrasena}, {new: true});
+        if (usuario != null){          
+        
+            transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
+            
+        }else{
+            console.log('No se encontró el usuario')
+        }
+        
+        return res.json({
+            status:'Si su correo es correcto, se le enviará un correo con su nueva contraseña'
+        });
+        
+    }catch(err){        
+        console.log(err)
+        res.json({
+            status:'Hubo un error en la operación'
+        })        
+    }
+})
+//elimina un usuario si concuerda el correo y la contraseña
+router.post('/eliminarUsuario', async (req,res) =>{
+    try{
+        const {correo,contrasena} = req.body;
+        await Usuario.findOneAndDelete({ correo,contrasena });
                
         res.json({
             status: true,
@@ -101,6 +161,9 @@ router.post('/eliminarUsuario', async (req,res) =>{
         });      
     }
 })
+
+
+
 
 
 module.exports = router;
