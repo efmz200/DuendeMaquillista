197eb2b893f51c9f4e1800fe6b4ac6e6a3be8b7b
deylan sandoval <deylan6@gmail.com>
2023-11-06 15:44:54 -0600

    profe ya no quiero

--- Codigo/frontendEve/src/pages/GaleriaDuende.jsx
+++ Codigo/frontendEve/src/pages/GaleriaDuende.jsx
@@ -311,33 +311,20 @@ const handleSubmit = (e) => {
           <a href="#!" class="flex items-center justify-center">
             <img
               class="mx-auto rounded-t-lg"
-              src="https://i.pinimg.com/564x/3c/73/93/3c7393c4d177db993866c2752cb0e708.jpg"
+              src={ contenidos[i].imagen}
               alt=""
             />
           </a>
           <div class="p-6">
-            <h5 class="mb-2 text-xl align-it font-medium leading-tight text-medGreen dark:text-neutral-50">
-              Nombre maquillaje
-            </h5>
+            
             <p class="mb-4 text-base text-medGreen dark:text-neutral-200 flex-wrap">
-              Descripción
+            { contenidos[i].descripcion}
             </p>
             <p class="mb-4 text-base text-medGreen dark:text-neutral-200">
-              #hashtag
+            { contenidos[i].tags}
             </p>
 
-            <form>
-              <div class="flex items-center px-3 py-2 rounded-lg bg-green dark:bg-medGreen">
-
-                <textarea id="chat" rows="1" class="block mx-4 p-4 w-full text-sm text-white bg-green rounded-lg border border-green focus:ring-green focus:border-green dark:bg-black dark:border-medGreen dark:placeholder-gray-400 dark:text-green dark:focus:ring-green dark:focus:border-green" placeholder="Tu mensaje..."></textarea>
-                <button type="submit" class="inline-flex justify-center p-2 text-green rounded-full cursor-pointer hover:bg-green dark:text-black dark:hover:bg-green">
-                  <svg class="w-5 h-5 rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                    <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z" />
-                  </svg>
-                  <span class="sr-only">Enviar mensaje</span>
-                </button>
-              </div>
-            </form>
+            
           </div>
 
           <div class="mb-4 flex items-center justify-center">
