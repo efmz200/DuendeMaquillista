1248390fb0133005560e09158202f31a3687d53c
Erick <efmz2000@outlook.es>
2023-11-18 20:13:50 -0600

    modelo de Agenda

---
+++ Codigo/src/models/modelo/Agenda.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const AgendaSchema = new Schema({
+    fecha: {type: Date, default: ''},
+    duracionHoras: {type: Number, default: 0},
+    duracionMinutos: {type: Number, default: 0},
+    asunto: {type: String, default: ''},
+    estado: {type: String, default: ''},
+    correoSolicitante: {type: String, default: ''},//correo
+});
+
+module.export = mongoose.model('Agenda',AgendaSchema);
