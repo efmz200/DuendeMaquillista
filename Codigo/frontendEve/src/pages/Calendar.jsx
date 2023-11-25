4e01a84a0f16c37305c88958ac33ae8f73fcf4d6
EveCS <ecruz1794@estudiantec.cr>
2023-11-24 19:56:20 -0600

    estilos ingresar eventos

--- Codigo/frontendEve/src/pages/Calendar.jsx
+++ Codigo/frontendEve/src/pages/Calendar.jsx
@@ -79,7 +79,7 @@ const Calendar = () => {
       setFormData({ title: '', start: '', end: '' });
     }
   };
-  
+
   // ------------------------------------------------- handle botones menu -------------------------------------------
 
   //Funciones para navegar entre menus
@@ -108,152 +108,156 @@ const Calendar = () => {
 
   return (
 
-    
-      <div className=" bg-black main-h-screen flex flex-col justify-center py-2">
-        
 
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <h2 className="text-center text-3xl font-regular bg-black text-white">
-            DUENDE MAQUILLISTA
-          </h2>
-          <br />
-        </div>
-
-        <div className="bg-medGreen flex flex-col justify-center py-2 sm:px-6 lg:px-8">
-          <div class="inline-flex rounded-md shadow-sm" role="group">
-            <button
-              onClick={handleGaleria}
-              type="button"
-              class="inline-flex items-center px-4 py-2 text-sm font-medium text-green bg-transparent border border-green rounded-l-lg hover:bg-green hover:text-white focus:z-10 focus:ring-2 focus:ring-green focus:bg-green focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-green dark:focus:bg-green"
-            >
-              La Galería del Duende
-            </button>
-            <button
-              onClick={handleAgenda}
-              type="button"
-              class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border-t border-b border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:bg-green dark:focus:bg-green"
-            >
-              La Agenda del Duende
-            </button>
-            <button
-              onClick={handleTienda}
-              type="button"
-              class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-r-md hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-green dark:focus:bg-green"
-            >
-              La Tienda del Duende
-            </button>
-
-            <div className="ml-auto flex space-x-2">
-              {" "}
-              {/* Utilizamos ml-auto para mover estos botones al lado derecho */}
-              <button
-                type="button"
-                //onClick={() => setShowModalEvento(true)}
-                class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-medGreen rounded-lg hover:bg-medGreen focus:ring-4 focus:outline-none focus:ring-green dark:bg-medGreen dark:hover:bg-green dark:focus:ring-green"
-              >
-                Agregar Evento
-              </button>
-              <button
-                onClick={handleMensajes}
-                type="button"
-                class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-medGreen rounded-lg hover:bg-medGreen focus:ring-4 focus:outline-none focus:ring-green dark:bg-medGreen dark:hover:bg-green dark:focus:ring-green"
-              >
-                Mensajes
-                <span class="inline-flex items-center justify-center w-4 h-4 ml-2 text-xs font-semibold text-white bg-green rounded-full">
-                  2
-                </span>
-              </button>
-              <button
-                onClick={handleMenuAdmin}
-                type="button"
-                class="text-white focus:ring-4 focus:ring-green font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-medgreen dark:hover:bg-green dark:focus:ring-green"
-              >
-                Menú Principal
-                <svg
-                  class="w-3.5 h-3.5 ml-2"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-              </button>
-            </div>
-
-          </div>
-        </div>
-
-        <div class="bg-white flex flex-col justify-center py-4 px-10 sm:px-24lg:px-8">
-          <div>
-            <div className="sm:mx-auto sm:w-full sm:max-w-md">
-              <h3 className="text-center text-xl font-regular">
-                LA AGENDA DEL DUENDE
-              </h3>
-            </div>
-
-            <div>
-              <hr className="my-2 h-0.5 border-t-0 bg-gray-400 opacity-100 dark:opacity-50" />
-            </div>
-
-            <div className="px-40 items-center justify-center">
-            <div className="mb-4">
-            
-            <form onSubmit={handleFormSubmit}>
-          <label>
-            Título del evento:
-            <input
-              type="text"
-              name="title"
-              value={formData.title}
-              onChange={handleInputChange}
-              required
-            />
-          </label>
-          <label>
-            Fecha y hora de inicio del evento:
-            <input
-              type="datetime-local"
-              name="start"
-              value={formData.start}
-              onChange={handleInputChange}
-              required
-            />
-          </label>
-          <label>
-            Fecha y hora de fin del evento:
-            <input
-              type="datetime-local"
-              name="end"
-              value={formData.end}
-              onChange={handleInputChange}
-              required
-            />
-          </label>
-          <button type="submit" className="bg-blue-500 text-white px-2 py-1 mr-2 rounded">
-            {selectedEvent ? 'Actualizar Evento' : 'Agregar Evento'}
-          </button>
-          {selectedEvent && (
-            <button
-              type="button"
-              onClick={handleEventDelete}
-              className="bg-red-500 text-white px-2 py-1 rounded"
-            >
-              Eliminar Evento
-            </button>
-          )}
-        </form>
+    <div className=" bg-black main-h-screen flex flex-col justify-center py-2">
 
 
-
-            
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="text-center text-3xl font-regular bg-black text-white">
+          DUENDE MAQUILLISTA
+        </h2>
+        <br />
       </div>
+
+      <div className="bg-medGreen flex flex-col justify-center py-2 sm:px-6 lg:px-8">
+        <div class="inline-flex rounded-md shadow-sm" role="group">
+          <button
+            onClick={handleGaleria}
+            type="button"
+            class="inline-flex items-center px-4 py-2 text-sm font-medium text-green bg-transparent border border-green rounded-l-lg hover:bg-green hover:text-white focus:z-10 focus:ring-2 focus:ring-green focus:bg-green focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-green dark:focus:bg-green"
+          >
+            La Galería del Duende
+          </button>
+          <button
+            onClick={handleAgenda}
+            type="button"
+            class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border-t border-b border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:bg-green dark:focus:bg-green"
+          >
+            La Agenda del Duende
+          </button>
+          <button
+            onClick={handleTienda}
+            type="button"
+            class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-r-md hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-green dark:focus:bg-green"
+          >
+            La Tienda del Duende
+          </button>
+
+          <div className="ml-auto flex space-x-2">
+            {" "}
+            {/* Utilizamos ml-auto para mover estos botones al lado derecho */}
+            <button
+              type="button"
+              //onClick={() => setShowModalEvento(true)}
+              class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-medGreen rounded-lg hover:bg-medGreen focus:ring-4 focus:outline-none focus:ring-green dark:bg-medGreen dark:hover:bg-green dark:focus:ring-green"
+            >
+              Agregar Evento
+            </button>
+            <button
+              onClick={handleMensajes}
+              type="button"
+              class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-medGreen rounded-lg hover:bg-medGreen focus:ring-4 focus:outline-none focus:ring-green dark:bg-medGreen dark:hover:bg-green dark:focus:ring-green"
+            >
+              Mensajes
+              <span class="inline-flex items-center justify-center w-4 h-4 ml-2 text-xs font-semibold text-white bg-green rounded-full">
+                2
+              </span>
+            </button>
+            <button
+              onClick={handleMenuAdmin}
+              type="button"
+              class="text-white focus:ring-4 focus:ring-green font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-medgreen dark:hover:bg-green dark:focus:ring-green"
+            >
+              Menú Principal
+              <svg
+                class="w-3.5 h-3.5 ml-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </button>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="bg-white flex flex-col justify-center py-4 px-10 sm:px-24lg:px-8">
+        <div>
+          <div className="sm:mx-auto sm:w-full sm:max-w-md">
+            <h3 className="text-center text-xl font-regular">
+              LA AGENDA DEL DUENDE
+            </h3>
+          </div>
+
+          <div>
+            <hr className="my-2 h-0.5 border-t-0 bg-gray-400 opacity-100 dark:opacity-50" />
+          </div>
+
+          <div className="px-40 items-center justify-center">
+            <div className="items-center justify-center py-4">
+
+              <form onSubmit={handleFormSubmit}>
+                <div>
+                <label className="mr-2">
+                  Evento:
+                  <input
+                    className="ml-2"
+                    type="text"
+                    name="title"
+                    value={formData.title}
+                    onChange={handleInputChange}
+                    required
+                  />
+                </label>
+                <label className="mr-8">
+                  Inicio:
+                  <input
+                    className="ml-2"
+                    type="datetime-local"
+                    name="start"
+                    value={formData.start}
+                    onChange={handleInputChange}
+                    required
+                  />
+                </label>
+                <label className="mr-2">
+                  Fin:
+                  <input 
+                  className="ml-2"
+                    type="datetime-local"
+                    name="end"
+                    value={formData.end}
+                    onChange={handleInputChange}
+                    required
+                  />
+                </label>
+                <button type="submit" className="bg-medGreen hover:bg-green text-white px-2 py-1 mr-4 ml-4 rounded">
+                  {selectedEvent ? 'Actualizar' : 'Agregar Evento'}
+                </button>
+                {selectedEvent && (
+                  <button
+                    type="button"
+                    onClick={handleEventDelete}
+                    className="bg-red-800 hover:bg-red-500 text-white px-3 py-1 rounded ml-2 "
+                  >
+                    Eliminar
+                  </button>
+                )}</div>
+              </form>
+
+
+
+
+            </div>
             <div className="mx-auto w-full max-w-lx" >
               <div className="calendar-container">
                 <FullCalendar
@@ -276,12 +280,12 @@ const Calendar = () => {
               </div>
             </div>
           </div>
-          </div><br/>
-        </div><br/><br/><br/><br/><br/>
+        </div><br />
+      </div><br /><br /><br /><br /><br />
 
-      </div>
-    
-    
+    </div>
+
+
   );
 };
 
